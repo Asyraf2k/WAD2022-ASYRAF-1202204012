f575fb3e85c1e1911e9214a99eaf26dc24d5292d
Asyraf2k <115558112+Asyraf2k@users.noreply.github.com>
2022-11-27 21:26:40 +0700

    modul4

---
+++ Modul4/Asyraf_HOME.php
@@ -0,0 +1,55 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>TP_MODUL3</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+</head>
+
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <div class="container">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-link" aria-current="page" href="Asyraf_home">Home</a>
+        <a class="nav-link active" href="Asyraf_list-car">MyCar</a>
+        <a href="Asyraf_add-car.php" style="width: 100px;position:relative;left:1000px;" class="btn btn-light fw-bold">Add Car</a>
+                </div>
+            </div>
+        </div>
+    </nav>
+    <br><br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <div class="clearfix">
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <b><h1><p class="fw-bold">Selamat Datang Di Show Room Asyraf</p></h1></b>
+                <p class="text-secondary" >At lacus vitae nulla sagittis scelerisque nist. Pellentesque duis curcus vestibulum, facilisi ac, sed faucibus.</p>
+            <br>
+            <div class="card-body">
+                <a class="nav-link text-secondary"><a href="Asyraf_list-car.php" class="btn btn-primary"> MyCar </a>
+            </div>
+            <div class="col">
+                <br><br><img src="image/logo-ead.png"  style="width: 100px;px;height: 25px;px;"><a> Asyraf_1202204012</a>
+            </div>
+            </div> 
+            <div class="col">
+                <img src="image/gambar1.png" style="width:600px;height:380px;">
+            </div>  
+        </div>
+    </div>
+    </div>
+    
+</body>
+
+</html>
